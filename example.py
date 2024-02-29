d59076824a25a3974b2f63e17126e2e8271595e9
CodeCraftJoca <seu-outro-email@example.com>
2024-02-29 08:55:31 -0300

    config: Adding pre-commit to project

--- example.py
+++ example.py
@@ -1,10 +1,5 @@
 """class exemple for test pylint"""
 
-def m_funcition():
-    '''Function description'''
-    x = 23
-    y = 7
-    z = 7
-    r = 10
-    b= 7
-    print('heloo')
+def minha_funcao():
+    """jjj"""
+    print("Olá, mundo!")
